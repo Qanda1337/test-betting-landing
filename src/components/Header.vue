cdca5dd375cb456ed70be0331e5f6eab4c311a6f
Qanda1337 <kingurushnik@gmail.com>
2025-11-21 17:23:37 +0300

    add yandex metrics

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="cursor-pointer bg-[var(--bg-card)]/0 w-full h-10 flex items-center justify-between px-[17%]">
-    <!-- Логотип / название -->
     <h1 class="text-[var(--text-main)] text-2xl font-bold">BetWins</h1>
 
-    <!-- Кнопка Log In -->
     <button
       class="bg-[var(--accent-primary)] text-[var(--bg-card)] font-semibold px-4 py-0.5 rounded-lg hover:bg-[var(--accent-secondary)] transition-colors duration-200 cursor-pointer"
       @click="showModal = true"
@@ -11,7 +9,6 @@
       Log In
     </button>
 
-    <!-- Модальное окно -->
     <RegistrationModal :visible="showModal" @close="showModal = false" />
   </header>
 </template>
